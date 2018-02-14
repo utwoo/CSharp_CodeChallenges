be0c2177df0b2c83f134c60bbf3d1bcc07c2c80e
utwoo <zhuxiang1212@hotmail.com>
2018-02-14 18:25:06 +0800

    Simple Encryption #1 - Alternating Split

---
+++ CSharp_CodeWar.Test/simple-encryption-number-1-alternating-split/SolutionTest.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using CSharp_CodeWar.simple_encryption_number_1_alternating_split;
+
+namespace CSharp_CodeWar.Test.simple_encryption_number_1_alternating_split
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void EncryptExampleTests()
+        {
+            Assert.Equal("This is a test!", Kata.Encrypt("This is a test!", 0));
+            Assert.Equal("hsi  etTi sats!", Kata.Encrypt("This is a test!", 1));
+            Assert.Equal("s eT ashi tist!", Kata.Encrypt("This is a test!", 2));
+            Assert.Equal(" Tah itse sits!", Kata.Encrypt("This is a test!", 3));
+            Assert.Equal("This is a test!", Kata.Encrypt("This is a test!", 4));
+            Assert.Equal("This is a test!", Kata.Encrypt("This is a test!", -1));
+            Assert.Equal("hskt svr neetn!Ti aai eyitrsig", Kata.Encrypt("This kata is very interesting!", 1));
+        }
+
+        [Fact]
+        public void DecryptExampleTests()
+        {
+            Assert.Equal("This is a test!", Kata.Decrypt("This is a test!", 0));
+            Assert.Equal("This is a test!", Kata.Decrypt("hsi  etTi sats!", 1));
+            Assert.Equal("This is a test!", Kata.Decrypt("s eT ashi tist!", 2));
+            Assert.Equal("This is a test!", Kata.Decrypt(" Tah itse sits!", 3));
+            Assert.Equal("This is a test!", Kata.Decrypt("This is a test!", 4));
+            Assert.Equal("This is a test!", Kata.Decrypt("This is a test!", -1));
+            Assert.Equal("This kata is very interesting!", Kata.Decrypt("hskt svr neetn!Ti aai eyitrsig", 1));
+        }
+
+        [Fact]
+        public void EmptyTests()
+        {
+            Assert.Equal("", Kata.Encrypt("", 0));
+            Assert.Equal("", Kata.Decrypt("", 0));
+        }
+
+        [Fact]
+        public void NullTests()
+        {
+            Assert.Null(Kata.Encrypt(null, 0));
+            Assert.Null(Kata.Decrypt(null, 0));
+        }
+    }
+}
---
+++ CSharp_CodeWar/simple-encryption-number-1-alternating-split/Kata.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace CSharp_CodeWar.simple_encryption_number_1_alternating_split
+{
+    public class Kata
+    {
+        public static string Encrypt(string text, int n)
+        {
+            if (string.IsNullOrEmpty(text) || n <= 0) return text;
+
+            while (n-- > 0)
+            {
+                text = string.Concat(text.Where((c, i) => (i % 2 == 1)).Concat(text.Where((c, i) => (i % 2 == 0))));
+            }
+
+            return text;
+        }
+
+        public static string Decrypt(string encryptedText, int n)
+        {
+            if (string.IsNullOrEmpty(encryptedText) || n <= 0) return encryptedText;
+
+            while (n-- > 0)
+            {
+                string leftpart = string.Concat(encryptedText.Take(encryptedText.Length / 2));
+                string rightpart = string.Concat(encryptedText.Skip(encryptedText.Length / 2));
+
+                encryptedText = string.Concat(Enumerable.Range(0, encryptedText.Length).Select(i => i % 2 == 0 ? rightpart[i / 2] : leftpart[i / 2]));
+            }
+            return encryptedText;
+        }
+    }
+}
