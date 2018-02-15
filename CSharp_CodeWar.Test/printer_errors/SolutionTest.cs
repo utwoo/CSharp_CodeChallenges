fde624b2d3ccf661634a505a1b090cb15efac4d3
utwoo <zhuxiang1212@hotmail.com>
2018-02-15 13:33:32 +0800

    printer errors

---
+++ CSharp_CodeWar.Test/printer_errors/SolutionTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using CSharp_CodeWar.printer_errors;
+
+namespace CSharp_CodeWar.Test.printer_errors
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public static void test()
+        {
+            string s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+            Assert.Equal("3/56", Kata.PrinterError(s));
+        }
+    }
+}
---
+++ CSharp_CodeWar/printer_errors/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+/// <summary>
+/// https://www.codewars.com/kata/56541980fa08ab47a0000040/train/csharp
+/// </summary>
+namespace CSharp_CodeWar.printer_errors
+{
+    public class Kata
+    {
+        public static string PrinterError(String s)
+        {
+            return string.Format("{0}/{1}", s.ToCharArray().ToList().Where(c => c > 'm').Count(), s.Length);
+        }
+    }
+}
