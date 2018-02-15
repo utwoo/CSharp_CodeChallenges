33693eb83075a739c3ef29f0684129eb10443f34
utwoo <zhuxiang1212@hotmail.com>
2018-02-15 14:09:31 +0800

    find-the-odd-int

---
+++ CSharp_CodeWar.Test/find-the-odd-int/SolutionTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using CSharp_CodeWar.find_the_odd_int;
+
+namespace CSharp_CodeWar.Test.find_the_odd_int
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void Tests()
+        {
+            Assert.Equal(5, Kata.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+        }
+    }
+}
---
+++ CSharp_CodeWar/find-the-odd-int/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace CSharp_CodeWar.find_the_odd_int
+{
+    public class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            return seq.ToArray().GroupBy(v => v).First(group => group.Count() % 2 == 1).Key;
+        }
+    }
+}
