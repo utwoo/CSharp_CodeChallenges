902c6c049cdd16abfcd371138d2d5db23d38d9e7
utwoo <zhuxiang1212@hotmail.com>
2018-02-19 13:33:27 +0800

    build_tower

---
+++ CSharp_CodeWar.Test/build-tower/SolutionTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using CSharp_CodeWar.build_tower;
+
+namespace CSharp_CodeWar.Test.build_tower
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void BasicTests()
+        {
+            Assert.Equal(string.Join(",", new[] { "*" }), string.Join(",", Kata.TowerBuilder(1)));
+            Assert.Equal(string.Join(",", new[] { " * ", "***" }), string.Join(",", Kata.TowerBuilder(2)));
+            Assert.Equal(string.Join(",", new[] { "  *  ", " *** ", "*****" }), string.Join(",", Kata.TowerBuilder(3)));
+        }
+    }
+}
---
+++ CSharp_CodeWar/build-tower/Kata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp_CodeWar.build_tower
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/576757b1df89ecf5bd00073b/csharp
+    /// </summary>
+    public static class Kata
+    {
+        #region Me
+        public static string[] TowerBuilder(int nFloors)
+        {
+            int count = 1 + (nFloors - 1) * 2;
+            var result = Enumerable.Range(1, nFloors).Select(i =>
+            {
+                return string.Format("{0}{1}{0}",
+                    string.Join("", Enumerable.Repeat(" ", (count - ((i - 1) * 2 + 1)) / 2).ToArray()),
+                    string.Join("", Enumerable.Repeat("*", ((i - 1) * 2 + 1)).ToArray()));
+            }).ToArray();
+
+            return result;
+        }
+        #endregion
+
+        #region Clever
+        //return Enumerable.Range(1, nFloors).Select(i => string.Format("{0}{1}{0}", i == nFloors? "" : new string (' ', nFloors - i), new string ('*', 2 * i - 1))).ToArray();
+        #endregion
+    }
+}
