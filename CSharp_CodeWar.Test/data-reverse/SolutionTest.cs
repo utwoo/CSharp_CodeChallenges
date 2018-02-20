5778f51ac0e2a1fa4e9e0ecb65eb827a48f0e20c
utwoo <zhuxiang1212@hotmail.com>
2018-02-20 15:43:45 +0800

    data_reverse

---
+++ CSharp_CodeWar.Test/data-reverse/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using CSharp_CodeWar.data_reverse;
+
+namespace CSharp_CodeWar.Test.data_reverse
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public static void test1()
+        {
+            int[] data1 = new int[32] { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0 };
+            int[] data2 = new int[32] { 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 };
+            Assert.Equal(data2, Kata.DataReverse(data1));
+        }
+
+        [Fact]
+        public static void test2()
+        {
+            int[] data1 = new int[16] { 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1 };
+            int[] data2 = new int[16] { 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0 };
+            Assert.Equal(data2, Kata.DataReverse(data1));
+        }
+    }
+}
---
+++ CSharp_CodeWar/data-reverse/Kata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp_CodeWar.data_reverse
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/569d488d61b812a0f7000015/csharp 
+    /// </summary>
+    public static class Kata
+    {
+        public static int[] DataReverse(int[] data)
+        {
+            #region Me
+            int length = data.Length;
+            List<int> result = new List<int>();
+            do
+            {
+                result.AddRange(data.Skip(length - 8).Take(8));
+                length -= 8;
+            } while (length > 0);
+
+            return result.ToArray();
+            #endregion
+
+            #region Clever
+            //int[] bits = data;
+
+            //for (int i = 0; i < data.Length; i += 8)
+            //    Array.Reverse(bits, i, 8);
+
+            //Array.Reverse(bits);
+            //return bits;
+            #endregion
+        }
+    }
+}
