a884487878daf5ae79a1a74bedc17f3f5238f9aa
utwoo <zhuxiang1212@hotmail.com>
2018-02-21 11:49:35 +0800

    count_characters_in_your_string

--- CSharp_CodeWar.Test/camelcase-method/SolutionTest.cs
+++ CSharp_CodeWar.Test/camelcase-method/SolutionTest.cs
@@ -1,19 +1,26 @@
-﻿using Xunit;
-using CSharp_CodeWar.camelcase_method;
+﻿using CSharp_CodeWar.count_characters_in_your_string;
+using System.Collections.Generic;
+using Xunit;
 
-namespace CSharp_CodeWar.Test.camelcase_method
+namespace CSharp_CodeWar.Test.count_characters_in_your_string
 {
     public class SolutionTest
     {
         [Fact]
-        public void BasicTests()
+        public static void FixedTest_aaaa()
         {
-            Assert.Equal("TestCase", "test case".CamelCase());
-            Assert.Equal("CamelCaseMethod", "camel case method".CamelCase());
-            Assert.Equal("SayHello", "say hello".CamelCase());
-            Assert.Equal("CamelCaseWord", " camel case word".CamelCase());
-            Assert.Equal("GcphgkTpjkvjlexkaqzIccKvU", "gcphgk  tpjkvjlexkaqz icc kv u ".CamelCase());
-            Assert.Equal("", "".CamelCase());
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('a', 4);
+            Assert.Equal(d, Kata.Count("aaaa"));
+        }
+
+        [Fact]
+        public static void FixedTest_aabb()
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('a', 2);
+            d.Add('b', 2);
+            Assert.Equal(d, Kata.Count("aabb"));
         }
     }
 }
---
+++ CSharp_CodeWar/count-characters-in-your-string/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp_CodeWar.count_characters_in_your_string
+{
+    /// <summary>
+    /// http://www.codewars.com/kata/52efefcbcdf57161d4000091/csharp
+    /// </summary>
+    public static class Kata
+    {
+        public static Dictionary<char, int> Count(string str)
+        {
+            #region Me
+            return str.GroupBy(c => c).ToDictionary(group => group.Key, group => group.Count());
+            #endregion
+
+            #region Best
+            //TextInfo cultInfo = new CultureInfo("en-US", false).TextInfo;
+            //str = cultInfo.ToTitleCase(str);
+            //str = str.Replace(" ", "");
+            //return str;
+            #endregion
+        }
+    }
+}
