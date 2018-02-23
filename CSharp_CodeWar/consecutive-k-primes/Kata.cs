ac764f2b5667ce7c30d9b011f11092f7929b413c
utwoo <zhuxiang1212@hotmail.com>
2018-02-23 17:54:58 +0800

    consecutive_k_primes

--- CSharp_CodeWar/consecutive-k-primes/Kata.cs
+++ CSharp_CodeWar/consecutive-k-primes/Kata.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharp_CodeWar.consecutive_k_primes
 {
@@ -7,10 +8,43 @@ namespace CSharp_CodeWar.consecutive_k_primes
     /// </summary>
     public static class Kata
     {
+        private static Dictionary<long, int> dicPrimeCount = new Dictionary<long, int>() { { 2, 1 }, { 3, 1 } };
+
         public static int ConsecKprimes(int k, long[] arr)
         {
-            // your code
-            return 0;
+            if (arr.Length < 2) return 0;
+
+            int result = 0;
+            int preCount = -1, count = -1;
+
+            foreach (long item in arr)
+            {
+                count = primeCount(item);
+                result += (count == k && preCount == k) ? 1 : 0;
+                preCount = count;
+            }
+
+            return result;
+        }
+
+        private static int primeCount(long value)
+        {
+            if (value <= 1) return 0;
+            if (dicPrimeCount.ContainsKey(value)) return dicPrimeCount[value];
+
+            int result = 0;
+            for (long i = 2; i <= Math.Sqrt(value); i++)
+            {
+                if (value % i == 0)
+                {
+                    result = primeCount(i) + primeCount(value / i);
+                    dicPrimeCount.Add(value, result);
+                    return result;
+                }
+            }
+
+            dicPrimeCount.Add(value, 1);
+            return 1;
         }
     }
 }
