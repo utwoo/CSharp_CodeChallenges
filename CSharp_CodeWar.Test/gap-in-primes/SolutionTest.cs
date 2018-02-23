035a7cb91a33bbf74a8bb4f79fdb22608b0f6bd9
utwoo <zhuxiang1212@hotmail.com>
2018-02-23 11:20:34 +0800

    gap_in_primes

---
+++ CSharp_CodeWar.Test/gap-in-primes/SolutionTest.cs
@@ -0,0 +1,18 @@
+﻿using CSharp_CodeWar.gap_in_primes;
+using Xunit;
+
+namespace CSharp_CodeWar.Test.gap_in_primes
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public static void test()
+        {
+            Assert.Equal(new long[] { 101, 103 }, Kata.Gap(2, 100, 110));
+            Assert.Equal(new long[] { 103, 107 }, Kata.Gap(4, 100, 110));
+            Assert.Null(Kata.Gap(6, 100, 110));
+            Assert.Equal(new long[] { 359, 367 }, Kata.Gap(8, 300, 400));
+            Assert.Equal(new long[] { 337, 347 }, Kata.Gap(10, 300, 400));
+        }
+    }
+}
---
+++ CSharp_CodeWar/gap-in-primes/Kata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CSharp_CodeWar.gap_in_primes
+{
+    public class Kata
+    {
+        public static long[] Gap(int g, long m, long n)
+        {
+            for (long i = m; i <= n - g; i++)
+            {
+                if (isPrime(i) && isPrime(i + g) && !isPrimeRange(i + 2, i + g - 2))
+                    return new long[2] { i, i + g };
+            }
+
+            return null;
+        }
+
+        private static bool isPrimeRange(long x, long y)
+        {
+            for (long i = x; i <= y; i++)
+                if (isPrime(i)) return true;
+
+            return false;
+        }
+
+        private static bool isPrime(long v)
+        {
+            if (v <= 3) return (v >= 2);
+            if (v % 2 == 0) return false;
+
+            for (long i = 3; i <= Math.Sqrt(v); i += 2)
+                if (v % i == 0) return false;
+
+            return true;
+        }
+    }
+}
