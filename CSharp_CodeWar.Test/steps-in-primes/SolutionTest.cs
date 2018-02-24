eda48fadd38fff35c3bbaa06e733dcfab14c9e8a
utwoo <zhuxiang1212@hotmail.com>
2018-02-24 10:17:11 +0800

    steps_in_primes

---
+++ CSharp_CodeWar.Test/steps-in-primes/SolutionTest.cs
@@ -0,0 +1,18 @@
+﻿using CSharp_CodeWar.steps_in_primes;
+using Xunit;
+
+namespace CSharp_CodeWar.Test.steps_in_primes
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public static void test1()
+        {
+            Assert.Equal(new long[] { 101, 103 }, Kata.Step(2, 100, 110));
+            Assert.Equal(new long[] { 103, 107 }, Kata.Step(4, 100, 110));
+            Assert.Equal(new long[] { 101, 107 }, Kata.Step(6, 100, 110));
+            Assert.Equal(new long[] { 359, 367 }, Kata.Step(8, 300, 400));
+            Assert.Equal(new long[] { 307, 317 }, Kata.Step(10, 300, 400));
+        }
+    }
+}
---
+++ CSharp_CodeWar/steps-in-primes/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// https://www.codewars.com/kata/steps-in-primes
+/// </summary>
+namespace CSharp_CodeWar.steps_in_primes
+{
+    public class Kata
+    {
+        public static long[] Step(int g, long m, long n)
+        {
+            for (long i = m; i <= n - g; i++)
+            {
+                if (isPrime(i) && isPrime(i + g))
+                    return new long[2] { i, i + g };
+            }
+
+            return null;
+        }
+
+        private static bool isPrime(long v)
+        {
+            if (v <= 3) return (v >= 2);
+            if (v % 2 == 0) return false;
+
+            for (long i = 3; i <= Math.Sqrt(v); i += 2)
+                if (v % i == 0) return false;
+
+            return true;
+        }
+    }
+}
