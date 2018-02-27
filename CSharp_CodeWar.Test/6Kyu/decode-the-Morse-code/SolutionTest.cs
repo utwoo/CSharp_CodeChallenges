4b122dd5e1a3213cb63fad20bccd9c6ee3122f9a
utwoo <zhuxiang1212@hotmail.com>
2018-02-27 21:45:42 +0800

    decode_the_Morse_code start

---
+++ CSharp_CodeWar.Test/6Kyu/decode-the-Morse-code/SolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using CSharp_CodeWar.decode_the_Morse_code;
+
+namespace CSharp_CodeWar.Test.decode_the_Morse_code
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void MorseCodeDecoderBasicTest_1()
+        {
+            string input = ".... . -.--   .--- ..- -.. .";
+            string expected = "HEY JUDE";
+
+            string actual = Kata.Decode(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CSharp_CodeWar/6Kyu/data-reverse/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp_CodeWar.decode_the_Morse_code
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/decode-the-morse-code/csharp
+    /// </summary>
+    public static class Kata
+    {
+        public static string Decode(string morseCode)
+        {
+            throw new System.NotImplementedException("Please provide some code.");
+        }
+    }
+}
