f5e2e13e80c5bbcdaecf3ec3c1a0693f11519f3e
utwoo <zhuxiang1212@hotmail.com>
2018-03-03 12:35:13 +0800

    LeetCode: Two Sum

---
+++ CSharp_LeetCode/P000/P001_TwoSum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// https://leetcode.com/problems/two-sum/
+/// </summary>
+namespace CSharp_LeetCode.P000
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (dic.ContainsKey(complement))
+                {
+                    return new int[] { dic[complement], i };
+                }
+
+                dic[nums[i]] = i;
+            }
+            return null;
+        }
+    }
+}
